126683c8a78d11aac43a50f3ad5fa477a7e130e3
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 13:00:51 +0300

    Rokas

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const App = () => {
           <View style={tailwind('pt-12 items-center')}>
               <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
                   <Text style={tailwind('text-blue-800 font-semibold')}>
-                      Hello Uber Driver App
+                      Uber Driver App
                   </Text>
               </View>
           </View>
