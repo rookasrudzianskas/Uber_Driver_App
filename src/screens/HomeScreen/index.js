7dda65fd0ca1040ae92ae7f2e9d4cefeb548fe80
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 15:49:14 +0300

    Rokas

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -8,16 +8,16 @@ import MapViewDirections from "react-native-maps-directions";
 const GOOGLE_MAPS_APIKEY = 'AIzaSyBmXijpsVGRk39hnHdg6aWoeZ_Uaj81B-Y';
 
 const HomeScreen = () => {
-    const origin = {latitude: 37.3318456, longitude: -122.0296002};
-    const destination = {latitude: 37.771707, longitude: -122.4053769};
+    const origin = {latitude: 54.7855097, longitude: 25.3463961};
+    const destination = {latitude: 54.7223604, longitude: 25.3022093};
 
     return (
             <View style={styles.container}>
                 <MapView
                     provider={PROVIDER_GOOGLE}
                     initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
+                        latitude: 54.7855097,
+                        longitude: 25.3463961,
                         latitudeDelta: 0.0922,
                         longitudeDelta: 0.0421,
                     }}
