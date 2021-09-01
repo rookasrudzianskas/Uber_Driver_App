a90c2f2d4ad976d6736702260599394b75f71e0c
Rokas <rookas.rudzenskas@gmail.com>
2021-09-01 11:36:48 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tailwind from 'tailwind-rn';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tailwind('flex flex-1 items-center justify-center bg-white')}>
+      <Text>UBER Driver app 🔥!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
