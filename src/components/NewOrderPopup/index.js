bf01fc861a5c8fe4155c0fa7f5e843d67972efcf
Rokas <rookas.rudzenskas@gmail.com>
2021-09-02 11:51:53 +0300

    Rokas

--- src/components/NewOrderPopup/index.js
+++ src/components/NewOrderPopup/index.js
@@ -22,7 +22,7 @@ const NewOrderPopup = ({ newOrder, onAccept, onDecline, duration, distance }) =>
             </View>
 
 
-            <View style={tailwind('bg-black absolute bottom-8 w-96 p-6 rounded-xl flex flex-col justify-center items-center justify-between')}>
+            <View onPress={onAccept} style={tailwind('bg-black absolute bottom-8 w-96 p-6 rounded-xl flex flex-col justify-center items-center justify-between')}>
                 <View style={tailwind("bg-black flex flex-row")}>
                     <View style={tailwind(" flex flex-row items-center mb-4")}>
                         <View style={tailwind("flex")}>
