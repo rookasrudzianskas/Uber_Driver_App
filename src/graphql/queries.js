708a9e5c638606592daac3c2deb375373093f933
Rokas <rookas.rudzenskas@gmail.com>
2021-09-13 09:48:08 +0300

    Rokas

---
+++ src/graphql/queries.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+
+export const getCar = /* GraphQL */ `
+    query GetCar($id: ID!) {
+        getCar(id: $id) {
+            id
+        }
+    }
+`;
